f998011bdbb003bafe117b323933bc54e7ef12cd
mushfiq123-git <mushfiq4167@gmail.com>
2026-02-26 20:06:05 +0530

    Production config

--- app.py
+++ app.py
@@ -255,5 +255,8 @@ def generate_invoice(sale_id):
 
     return send_file(filename, as_attachment=True)
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
